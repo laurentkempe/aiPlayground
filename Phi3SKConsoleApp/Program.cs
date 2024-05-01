c9bac8a0e19f685561f84f3cd6bc2611e505198f
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2024-05-01 10:14:39 +0200

    Add Phi3SKConsoleApp

---
+++ Phi3SKConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.SemanticKernel;
+using System.Text;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+// Initialize the Semantic kernel
+var kernelBuilder = Kernel.CreateBuilder();
+var kernel = kernelBuilder
+    .AddOpenAIChatCompletion(                        // We use Semantic Kernel OpenAI API
+        modelId: "phi3",
+        apiKey: null,
+        endpoint: new Uri("http://localhost:11434")) // With Ollama OpenAI API endpoint
+    .Build();
+
+// Create a new chat
+var ai = kernel.GetRequiredService<IChatCompletionService>();
+ChatHistory chat = new("You are an AI assistant that helps people find information.");
+StringBuilder builder = new();
+
+// User question & answer loop
+while (true)
+{
+    Console.Write("Question: ");
+    chat.AddUserMessage(Console.ReadLine()!);
+
+    builder.Clear();
+
+    // Get the AI response streamed back to the console
+    await foreach (var message in ai.GetStreamingChatMessageContentsAsync(chat, kernel: kernel))
+    {
+        Console.Write(message);
+        builder.Append(message.Content);
+    }
+    Console.WriteLine();
+    chat.AddAssistantMessage(builder.ToString());
+
+    Console.WriteLine();
+}
