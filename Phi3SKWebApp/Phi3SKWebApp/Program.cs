12d482b5b9e5b437903a0b9ad0e4843f9decd499
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2024-05-08 12:31:35 +0200

    Add Phi3SKWebApp project

---
+++ Phi3SKWebApp/Phi3SKWebApp/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.FluentUI.AspNetCore.Components;
+using Microsoft.SemanticKernel;
+using Phi3SKWebApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddFluentUIComponents();
+
+// Add Semantic Kernel services to the container.
+var kernelBuilder = builder.Services.AddKernel();
+kernelBuilder.AddOpenAIChatCompletion("phi3:latest", apiKey: null, endpoint: new Uri("http://localhost:11434"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ Phi3SKWebApp/README.md
@@ -0,0 +1,5 @@
+# Phi3SKWebApp
+
+A blazor web application leveraging [Fluent UI Blazor components library](https://github.com/microsoft/fluentui-blazor) that interacts with [Phi-3](https://azure.microsoft.com/en-us/blog/introducing-phi-3-redefining-whats-possible-with-slms/) on your machine using [Ollama](https://www.ollama.com/), C# and [Semantic Kernel](https://github.com/microsoft/semantic-kernel). It demonstrates the streaming of chat completion from Phi-3 to the web application in real-time.
+
+You can read about this on my blog post [TODO](https://laurentkempe.com/).
